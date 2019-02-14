84336d79ba7436267cde7f5c851c36cd830e2bd8
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-02-14 13:00:48 -0500

    Tidy compileDKE print output

--- DKE/compileDKE.m
+++ DKE/compileDKE.m
@@ -34,18 +34,19 @@ end
 gradientDir = ['.' filesep 'gradientVectors'];
 
 %   Run compiler
-fprintf('1. Compiling DKE (1/3)\n\n');
+fprintf('1. Compiling DKE (1/3)\n');
 mcc('-v','-m','dke.m','-o','dke','-a',gradientDir,...
     '-a',mexFolder,'-d',folderName);
+fprintf('\n\n');
 
-fprintf('2. Compiling dke_preprocess_dicom (2/3)\n\n');
+fprintf('2. Compiling dke_preprocess_dicom (2/3)\n');
 mcc('-v','-m','dke_preprocess_dicom.m','-o','dke_preprocess_dicom',...
     '-a','spm_dicom_dict.mat','-d',folderName);
+fprintf('\n\n');
 
-fprintf('3. Compiling map_interpolate (3/3)\n\n');
+fprintf('3. Compiling map_interpolate (3/3)\n');
 mcc('-v','-m','map_interpolate.m','-o','map_interpolation',...
     '-d',folderName);
-
-fprintf('\n');
+fprintf('\n\n');
 fprintf('completed\n\n')
 end
